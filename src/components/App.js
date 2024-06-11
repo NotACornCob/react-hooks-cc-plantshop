2292ee883ee95b3207f395f87ef25793209c68e9
NotACornCob <alex.kleinman@gmail.com>
2024-06-11 10:54:44 -0400

    FETCH template

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,12 @@ import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  useEffect(() => {
+    fetch("/api")
+      .then((r) => r.json())
+      .then(setData);
+  }, []);
+  
   return (
     <div className="app">
       <Header />
--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,37 @@
 import React from "react";
 
-function NewPlantForm() {
+function NewPlantForm(onSubmit) {
+  const [formData, setFormData] = useState({
+    plantname: "",
+    imageURL: "",
+    Price: "",
+  });
+  
+  function handleChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch("the server URL", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Plant name" onChange={handleChange} value={formData.plantname} />
+        <input type="text" name="image" placeholder="Image URL" onChange={handleChange} value={formData.imageURL} />
+        <input type="number" name="price" step="0.01" placeholder="Price" onChange={handleChange} value={formData.price} />
         <button type="submit">Add Plant</button>
       </form>
     </div>
