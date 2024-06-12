d889e589306863f48f83a4c3e11258167f7a436c
Alex <alex.kleinman@gmail.com>
2024-06-12 13:09:21 -0400

    completed challenge

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,25 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants, search, userPlantObjects}) {
+  const searchLowerCase = search.toLowerCase();
+  const plantsToDisplay = plants.filter((plant) => {
+    const plantNameLowerCase = plant.name.toLowerCase();
+    return plantNameLowerCase.includes(searchLowerCase);
+  });
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plantsToDisplay.map((plant) => (
+      < PlantCard key={plant.id} name={plant.name} image={plant.image} price={plant.price}/>))}
+      {userPlantObjects? (
+        < PlantCard key={userPlantObjects.id} name={userPlantObjects.name} image={userPlantObjects.image} price={userPlantObjects.price}/>
+      ) : (
+        null
+      )
+      }
+    </ul>
+      
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,16 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+import { v4 as uuid } from "uuid";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("")
+  const [submittedData, setSubmittedData] = useState()
+  const [plantName, setPlantName] = useState("")
+  const [plantImage, setPlantImage] = useState("")
+  const [plantPrice, setPlantPrice] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((plants) => {setPlants(plants)});
+  }, []);
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    const formData = {
+      name: plantName || "Audrey II",
+      image: plantImage || "https://vignette.wikia.nocookie.net/warehouse-13-artifact-database/images/c/c2/Shop_of_horrors.jpg/revision/latest?cb=20190618141718",
+      price: plantPrice || "1.95"
+    };
+  
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/JSON",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((newPlant) => {
+        setPlants((prevPlants) => [...prevPlants, newPlant]);
+        setPlantName("");
+        setPlantImage("");
+        setPlantPrice("");
+        
+      })
+  };
+
+  const handleImage = (e) => {
+    setPlantImage(e.target.value);
+  }
+
+  const handleName = (e) => {
+    console.log(e.target.value)
+    setPlantName(e.target.value)
+  }
+
+  const handlePrice = (e) => {
+    console.log(e.target.value)
+    setPlantPrice(e.target.value)
+  }
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onFormSubmit={handleSubmit} onNameChange={handleName} onImageChange={handleImage} onPriceChange={handlePrice} />
+      <Search handleSearch={handleSearch}/>
+      <PlantList plants={plants} search={search} userPlantObjects={submittedData}/>
     </main>
   );
 }
+
 
 export default PlantPage;
